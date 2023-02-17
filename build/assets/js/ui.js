f48960c8ce88ced78d3fb178beb4dc459558e1dc
pcft9507 <31539142+pcft9507@users.noreply.github.com>
2023-02-17 17:23:00 +0900

    약관동의, comming soon

--- build/assets/js/ui.js
+++ build/assets/js/ui.js
@@ -1,25 +1,20 @@
+
 // 로그인 열기
-function openLogin(type) {
-  const loginType = type;
-  if (loginType === 'expert') {
-    $('.l-login-wrap.expert').stop().fadeIn(200);
-  } else {
-    $('.l-login-wrap.user').stop().fadeIn(200);
-  }
+function openModal(name) {
+  const modalName = name;
+  $('.l-modal[data-modal="' + name + '"]').stop().fadeIn(200);
+  $('html').addClass('ovh');
 }
 
 // 로그인 닫기
-function closeLogin() {
-  $('.l-login-wrap').stop().fadeOut(200);
+function closeModal(name) {
+  $('.l-modal[data-modal="'+ name +'"]').stop().fadeOut(200);
+  $('html').removeClass('ovh');
 }
-
 
 // 문서 로드 후 실행
 $(document).ready(function () {
-  // 헤더 초기화
-  var $headerSub = $('.l-header-sub');
-
-  // 하단메뉴 현재 뎁스 활성화
+  // 하단메뉴 상단 뎁스 활성화
   if (typeof page != 'undefined') {
     if (page == 'home') {
       $('.bQuickMenu__item').eq(0).find('.bQuickMenu__txt').addClass('on');
